91e777239f53d65367ca36fc4bcefb9718fec5ed
231091 - JOSE JAVIER TORRES MARTINEZ <jtorresm6@miumg.edu.gt>
2026-09-09 22:43:05 -0600

    Cambios Vista

---
+++ prototipoumg2k26/CapaVista_prototipo2k26/Formas/FrmEmpleados.Designer.cs
@@ -0,0 +1,405 @@
+﻿namespace CapaVista_prototipo2k26.Formas
+{
+    partial class FrmEmpleados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.dgvEmpleados = new System.Windows.Forms.DataGridView();
+            this.btnAyuda = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtContrataacion = new System.Windows.Forms.DateTimePicker();
+            this.txtNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.txtDpi = new System.Windows.Forms.TextBox();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.txtPuesto = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNit = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtIdEmpleado = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.lblPuesto = new System.Windows.Forms.Label();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.lblFechaContratacion = new System.Windows.Forms.Label();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblFechaNaci = new System.Windows.Forms.Label();
+            this.LblApellido = new System.Windows.Forms.Label();
+            this.lblNombEmpleado = new System.Windows.Forms.Label();
+            this.lblNitEmpleado = new System.Windows.Forms.Label();
+            this.lblDpiEmpleado = new System.Windows.Forms.Label();
+            this.lblIdEmpleado = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(63, 29);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(705, 22);
+            this.txtBuscar.TabIndex = 0;
+            // 
+            // dgvEmpleados
+            // 
+            this.dgvEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmpleados.Location = new System.Drawing.Point(44, 71);
+            this.dgvEmpleados.Name = "dgvEmpleados";
+            this.dgvEmpleados.RowHeadersWidth = 51;
+            this.dgvEmpleados.RowTemplate.Height = 24;
+            this.dgvEmpleados.Size = new System.Drawing.Size(805, 302);
+            this.dgvEmpleados.TabIndex = 1;
+            // 
+            // btnAyuda
+            // 
+            this.btnAyuda.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAyuda.Location = new System.Drawing.Point(467, 390);
+            this.btnAyuda.Name = "btnAyuda";
+            this.btnAyuda.Size = new System.Drawing.Size(75, 23);
+            this.btnAyuda.TabIndex = 2;
+            this.btnAyuda.Text = "Ayuda";
+            this.btnAyuda.UseVisualStyleBackColor = false;
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnImprimir.Location = new System.Drawing.Point(548, 390);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btnImprimir.TabIndex = 3;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = false;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnNuevo.Location = new System.Drawing.Point(629, 391);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 4;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = false;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.SystemColors.Info;
+            this.btnEditar.Location = new System.Drawing.Point(710, 390);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 5;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.DeepPink;
+            this.btnBorrar.Location = new System.Drawing.Point(793, 390);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 6;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtContrataacion);
+            this.panel1.Controls.Add(this.txtNacimiento);
+            this.panel1.Controls.Add(this.txtDpi);
+            this.panel1.Controls.Add(this.txtEstado);
+            this.panel1.Controls.Add(this.txtPuesto);
+            this.panel1.Controls.Add(this.txtDireccion);
+            this.panel1.Controls.Add(this.txtApellido);
+            this.panel1.Controls.Add(this.txtNit);
+            this.panel1.Controls.Add(this.txtNombre);
+            this.panel1.Controls.Add(this.txtIdEmpleado);
+            this.panel1.Controls.Add(this.btnGuardar);
+            this.panel1.Controls.Add(this.lblPuesto);
+            this.panel1.Controls.Add(this.lblEstado);
+            this.panel1.Controls.Add(this.lblFechaContratacion);
+            this.panel1.Controls.Add(this.lblDireccion);
+            this.panel1.Controls.Add(this.lblFechaNaci);
+            this.panel1.Controls.Add(this.LblApellido);
+            this.panel1.Controls.Add(this.lblNombEmpleado);
+            this.panel1.Controls.Add(this.lblNitEmpleado);
+            this.panel1.Controls.Add(this.lblDpiEmpleado);
+            this.panel1.Controls.Add(this.lblIdEmpleado);
+            this.panel1.Location = new System.Drawing.Point(874, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 393);
+            this.panel1.TabIndex = 7;
+            // 
+            // txtContrataacion
+            // 
+            this.txtContrataacion.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtContrataacion.Location = new System.Drawing.Point(136, 247);
+            this.txtContrataacion.Name = "txtContrataacion";
+            this.txtContrataacion.Size = new System.Drawing.Size(134, 22);
+            this.txtContrataacion.TabIndex = 22;
+            // 
+            // txtNacimiento
+            // 
+            this.txtNacimiento.CustomFormat = "";
+            this.txtNacimiento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtNacimiento.Location = new System.Drawing.Point(123, 183);
+            this.txtNacimiento.Name = "txtNacimiento";
+            this.txtNacimiento.Size = new System.Drawing.Size(150, 22);
+            this.txtNacimiento.TabIndex = 21;
+            // 
+            // txtDpi
+            // 
+            this.txtDpi.Location = new System.Drawing.Point(107, 51);
+            this.txtDpi.Name = "txtDpi";
+            this.txtDpi.Size = new System.Drawing.Size(163, 22);
+            this.txtDpi.TabIndex = 20;
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(110, 281);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(160, 22);
+            this.txtEstado.TabIndex = 18;
+            // 
+            // txtPuesto
+            // 
+            this.txtPuesto.Location = new System.Drawing.Point(110, 309);
+            this.txtPuesto.Name = "txtPuesto";
+            this.txtPuesto.Size = new System.Drawing.Size(160, 22);
+            this.txtPuesto.TabIndex = 17;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(107, 214);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(163, 22);
+            this.txtDireccion.TabIndex = 15;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(107, 151);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(163, 22);
+            this.txtApellido.TabIndex = 13;
+            // 
+            // txtNit
+            // 
+            this.txtNit.Location = new System.Drawing.Point(107, 85);
+            this.txtNit.Name = "txtNit";
+            this.txtNit.Size = new System.Drawing.Size(163, 22);
+            this.txtNit.TabIndex = 12;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(110, 121);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(163, 22);
+            this.txtNombre.TabIndex = 11;
+            // 
+            // txtIdEmpleado
+            // 
+            this.txtIdEmpleado.Location = new System.Drawing.Point(107, 23);
+            this.txtIdEmpleado.Name = "txtIdEmpleado";
+            this.txtIdEmpleado.Size = new System.Drawing.Size(163, 22);
+            this.txtIdEmpleado.TabIndex = 10;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(110, 345);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 39);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // lblPuesto
+            // 
+            this.lblPuesto.AutoSize = true;
+            this.lblPuesto.Location = new System.Drawing.Point(13, 311);
+            this.lblPuesto.Name = "lblPuesto";
+            this.lblPuesto.Size = new System.Drawing.Size(49, 16);
+            this.lblPuesto.TabIndex = 9;
+            this.lblPuesto.Text = "Puesto";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(13, 284);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(50, 16);
+            this.lblEstado.TabIndex = 8;
+            this.lblEstado.Text = "Estado";
+            // 
+            // lblFechaContratacion
+            // 
+            this.lblFechaContratacion.AutoSize = true;
+            this.lblFechaContratacion.Location = new System.Drawing.Point(3, 252);
+            this.lblFechaContratacion.Name = "lblFechaContratacion";
+            this.lblFechaContratacion.Size = new System.Drawing.Size(127, 16);
+            this.lblFechaContratacion.TabIndex = 7;
+            this.lblFechaContratacion.Text = "Fecha_Contratacion";
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(13, 220);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(64, 16);
+            this.lblDireccion.TabIndex = 6;
+            this.lblDireccion.Text = "Direccion";
+            // 
+            // lblFechaNaci
+            // 
+            this.lblFechaNaci.AutoSize = true;
+            this.lblFechaNaci.Location = new System.Drawing.Point(-3, 188);
+            this.lblFechaNaci.Name = "lblFechaNaci";
+            this.lblFechaNaci.Size = new System.Drawing.Size(120, 16);
+            this.lblFechaNaci.TabIndex = 5;
+            this.lblFechaNaci.Text = "Fecha_Nacimiento";
+            // 
+            // LblApellido
+            // 
+            this.LblApellido.AutoSize = true;
+            this.LblApellido.Location = new System.Drawing.Point(13, 157);
+            this.LblApellido.Name = "LblApellido";
+            this.LblApellido.Size = new System.Drawing.Size(57, 16);
+            this.LblApellido.TabIndex = 4;
+            this.LblApellido.Text = "Apellido";
+            // 
+            // lblNombEmpleado
+            // 
+            this.lblNombEmpleado.AutoSize = true;
+            this.lblNombEmpleado.Location = new System.Drawing.Point(13, 124);
+            this.lblNombEmpleado.Name = "lblNombEmpleado";
+            this.lblNombEmpleado.Size = new System.Drawing.Size(56, 16);
+            this.lblNombEmpleado.TabIndex = 3;
+            this.lblNombEmpleado.Text = "Nombre";
+            // 
+            // lblNitEmpleado
+            // 
+            this.lblNitEmpleado.AutoSize = true;
+            this.lblNitEmpleado.Location = new System.Drawing.Point(13, 88);
+            this.lblNitEmpleado.Name = "lblNitEmpleado";
+            this.lblNitEmpleado.Size = new System.Drawing.Size(93, 16);
+            this.lblNitEmpleado.TabIndex = 2;
+            this.lblNitEmpleado.Text = "Nit_Empleado";
+            // 
+            // lblDpiEmpleado
+            // 
+            this.lblDpiEmpleado.AutoSize = true;
+            this.lblDpiEmpleado.Location = new System.Drawing.Point(3, 53);
+            this.lblDpiEmpleado.Name = "lblDpiEmpleado";
+            this.lblDpiEmpleado.Size = new System.Drawing.Size(98, 16);
+            this.lblDpiEmpleado.TabIndex = 1;
+            this.lblDpiEmpleado.Text = "Dpi_Empleado";
+            // 
+            // lblIdEmpleado
+            // 
+            this.lblIdEmpleado.AutoSize = true;
+            this.lblIdEmpleado.Location = new System.Drawing.Point(13, 26);
+            this.lblIdEmpleado.Name = "lblIdEmpleado";
+            this.lblIdEmpleado.Size = new System.Drawing.Size(88, 16);
+            this.lblIdEmpleado.TabIndex = 0;
+            this.lblIdEmpleado.Text = "id_Empleado";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(774, 29);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 8;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // FrmEmpleados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1237, 450);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnAyuda);
+            this.Controls.Add(this.dgvEmpleados);
+            this.Controls.Add(this.txtBuscar);
+            this.Name = "FrmEmpleados";
+            this.Text = "01005 Manteimiento Empleados - Prototipo v 0.5.0";
+            this.Load += new System.EventHandler(this.FrmEmpleados_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.DataGridView dgvEmpleados;
+        private System.Windows.Forms.Button btnAyuda;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblNombEmpleado;
+        private System.Windows.Forms.Label lblNitEmpleado;
+        private System.Windows.Forms.Label lblDpiEmpleado;
+        private System.Windows.Forms.Label lblIdEmpleado;
+        private System.Windows.Forms.TextBox txtDpi;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.TextBox txtPuesto;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNit;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtIdEmpleado;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label lblPuesto;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Label lblFechaContratacion;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblFechaNaci;
+        private System.Windows.Forms.Label LblApellido;
+        private System.Windows.Forms.DateTimePicker txtNacimiento;
+        private System.Windows.Forms.DateTimePicker txtContrataacion;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
---
+++ prototipoumg2k26/CapaVista_prototipo2k26/Formas/FrmEmpleados.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador_prototipo2k26;
+
+namespace CapaVista_prototipo2k26.Formas
+{
+    public partial class FrmEmpleados : Form
+    {
+        private ModeloEmpleado empleado = new ModeloEmpleado();
+
+        public FrmEmpleados()
+        {
+            InitializeComponent();
+            panel1.Enabled = false;
+        }
+
+        private void FrmEmpleados_Load(object sender, EventArgs e)
+        {
+            listaEmpleados();
+        }
+
+        private void listaEmpleados()
+        {
+            try
+            {
+                dgvEmpleados.DataSource = empleado.GetAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al listar empleados: " + ex.Message);
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            dgvEmpleados.DataSource = empleado.FindbyId(txtBuscar.Text);
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            empleado.dpi_emp = txtDpi.Text;
+            empleado.nit_emp = txtNit.Text;
+            empleado.nombre_emp = txtNombre.Text;
+            empleado.apellido_emp = txtApellido.Text;
+            empleado.fecha_nacimiento = txtNacimiento.Value;
+            empleado.direccion_emp = txtDireccion.Text;
+            empleado.fecha_contratacion = txtContrataacion.Value;
+            empleado.estado_emp = txtEstado.Text;
+            empleado.id_puesto = string.IsNullOrEmpty(txtPuesto.Text) ? 0 : Convert.ToInt32(txtPuesto.Text);
+
+            bool valido = new Ayudas.ValidacionDatos(empleado).Validar();
+            if (valido)
+            {
+                string resultado = empleado.GrabarCambios();
+                MessageBox.Show(resultado);
+                listaEmpleados();
+                Reinicio();
+            }
+        }
+
+        private void Reinicio()
+        {
+            panel1.Enabled = false;
+            txtIdEmpleado.Clear();
+            txtDpi.Clear();
+            txtNit.Clear();
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtNacimiento.Value = DateTime.Today;
+            txtDireccion.Clear();
+            txtContrataacion.Value = DateTime.Today;
+            txtEstado.Clear();
+            txtPuesto.Clear();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = true;
+            empleado.Estado = EstadoEntidad.Added;
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dgvEmpleados.SelectedRows.Count > 0)
+            {
+                panel1.Enabled = true;
+                empleado.Estado = EstadoEntidad.Modified;
+
+                empleado.id_empleado = Convert.ToInt32(dgvEmpleados.CurrentRow.Cells[0].Value);
+                txtIdEmpleado.Text = dgvEmpleados.CurrentRow.Cells[0].Value.ToString();
+                txtDpi.Text = dgvEmpleados.CurrentRow.Cells[1].Value.ToString();
+                txtNit.Text = dgvEmpleados.CurrentRow.Cells[2].Value.ToString();
+                txtNombre.Text = dgvEmpleados.CurrentRow.Cells[3].Value.ToString();
+                txtApellido.Text = dgvEmpleados.CurrentRow.Cells[4].Value.ToString();
+                txtNacimiento.Value = Convert.ToDateTime(dgvEmpleados.CurrentRow.Cells[5].Value);
+                txtDireccion.Text = dgvEmpleados.CurrentRow.Cells[6].Value.ToString();
+                txtContrataacion.Value = Convert.ToDateTime(dgvEmpleados.CurrentRow.Cells[7].Value);
+                txtEstado.Text = dgvEmpleados.CurrentRow.Cells[8].Value.ToString();
+                txtPuesto.Text = dgvEmpleados.CurrentRow.Cells[9].Value.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una fila");
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (dgvEmpleados.SelectedRows.Count > 0)
+            {
+                empleado.Estado = EstadoEntidad.Deleted;
+                empleado.id_empleado = Convert.ToInt32(dgvEmpleados.CurrentRow.Cells[0].Value);
+                string resultado = empleado.GrabarCambios();
+                MessageBox.Show(resultado);
+                listaEmpleados();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una fila");
+            }
+        }
+    }
+}
